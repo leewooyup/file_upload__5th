5b5c3afe181a63d35fc2a9798d00be02de421508
leewooyup <zjadlspun1114@naver.com>
2023-02-03 14:40:29 +0900

    파일업로드폼

--- app0203/src/main/java/com/ll/exam/app0203/app/home/controller/HomeController.java
+++ app0203/src/main/java/com/ll/exam/app0203/app/home/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String main() {
         return "home/main";
     }
+
+    @RequestMapping("/test/upload")
+    public String upload() {
+        return "home/test/upload";
+    }
 }
