d9b4bcbf67f19162a7921b76ea068ed2b73db4a9
leewooyup <zjadlspun1114@naver.com>
2023-02-03 17:13:12 +0900

    로그인 정보를 프로파일 페이지에서 보여줌

--- app0203/src/main/java/com/ll/exam/app0203/app/member/controller/MemberController.java
+++ app0203/src/main/java/com/ll/exam/app0203/app/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.ll.exam.app0203.app.member.entity.Member;
 import com.ll.exam.app0203.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +38,7 @@ public class MemberController {
     }
 
     @GetMapping("/profile")
-    public String showProfile(HttpSession session) {
+    public String showProfile(HttpSession session, Model model) {
         Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
         boolean isLogined = loginedMemberId != null;
 
@@ -45,6 +46,8 @@ public class MemberController {
             return "redirect:/?errorMsg=Need to login!";
         }
 
+        Member loginedMember = memberService.getMemberById(loginedMemberId);
+        model.addAttribute("loginedMember", loginedMember);
         return "member/profile";
     }
 }
