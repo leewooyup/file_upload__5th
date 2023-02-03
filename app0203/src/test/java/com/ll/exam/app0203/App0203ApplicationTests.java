c39d5632e309ab9cd4df11f2d02b9963981cd852
leewooyup <zjadlspun1114@naver.com>
2023-02-03 17:28:00 +0900

    GET / 요청의 응답본문에 안녕이 포함

--- app0203/src/test/java/com/ll/exam/app0203/App0203ApplicationTests.java
+++ app0203/src/test/java/com/ll/exam/app0203/App0203ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ll.exam.app0203;
 
+import com.ll.exam.app0203.app.home.controller.HomeController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class App0203ApplicationTests {
-
+	@Autowired
+	private MockMvc mvc;
 	@Test
-	void contextLoads() {
+	@DisplayName("메인화면에서는 안녕이 나와야 한다.")
+	void t1() throws Exception {
+		ResultActions resultActions = mvc
+				.perform(get("/"))
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(HomeController.class))
+				.andExpect(handler().methodName("main"))
+				.andExpect(content().string(containsString("안녕")));
 	}
 
 }
