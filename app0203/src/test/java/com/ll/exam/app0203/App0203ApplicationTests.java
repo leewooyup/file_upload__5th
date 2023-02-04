46c5f55eeea04f1e52843c56d9b22569f5988aa3
leewooyup <zjadlspun1114@naver.com>
2023-02-04 12:40:32 +0900

    테스트 환경의 샘플데이터 생성, 테스트 db 분리

--- app0203/src/test/java/com/ll/exam/app0203/App0203ApplicationTests.java
+++ app0203/src/test/java/com/ll/exam/app0203/App0203ApplicationTests.java
@@ -1,16 +1,20 @@
 package com.ll.exam.app0203;
 
 import com.ll.exam.app0203.app.home.controller.HomeController;
+import com.ll.exam.app0203.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -19,9 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles({"base-addi", "test"})
 class App0203ApplicationTests {
 	@Autowired
 	private MockMvc mvc;
+
+	@Autowired
+	private MemberService memberService;
+
 	@Test
 	@DisplayName("메인화면에서는 안녕이 나와야 한다.")
 	void t1() throws Exception {
@@ -34,6 +43,14 @@ class App0203ApplicationTests {
 				.andExpect(handler().handlerType(HomeController.class))
 				.andExpect(handler().methodName("main"))
 				.andExpect(content().string(containsString("안녕")));
+	}
+
+	@Test
+	@DisplayName("회원의 수")
+	@Rollback(false)
+	void t2() throws Exception {
+		long count = memberService.count();
+		assertThat(count).isGreaterThan(0);
 	}
 
 }
